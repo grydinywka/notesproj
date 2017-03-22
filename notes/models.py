fd27891b2705f7ac9dd2e63a76b79ff11cabe94f
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-22 19:45:05 +0200

    model Note

--- notes/models.py
+++ notes/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    """ Model for Notes """
+    text = models.TextField()
+
+    def __str__(self):
+        length = len(self.text.split())
+        if length > 2:
+            return " ".join(self.text.split()[:2]) + " ..."
+        else:
+            return self.text
