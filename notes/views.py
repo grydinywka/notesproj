df56272d93c1c48a62c17d51fd5223eaab2b382e
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-23 22:35:31 +0200

    create form for add notes

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,25 @@
-from django.views.generic import ListView
-
+from django.views.generic import ListView, CreateView
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from notes.models import Note
 
 
 class NoteListView(ListView):
     model = Note
     context_object_name = "notes"
+
+
+class NoteCreateView(CreateView):
+    model = Note
+    fields = ('text',)
+
+    def get_success_url(self):
+        messages.success(self.request, "Note create successfully")
+        return reverse('home')
+
+    def form_valid(self, form):
+        if self.request.POST.get("cancel_button"):
+            messages.success(self.request, "Note is not create")
+            return HttpResponseRedirect(reverse('home'))
+        return super(NoteCreateView, self).form_valid(form)
--- notes_proj/urls.py
+++ notes_proj/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from notes.views import NoteListView
+from notes.views import NoteListView, NoteCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', NoteListView.as_view(), name='home'),
+    url(r'^note/add/$', NoteCreateView.as_view(), name='notes_add'),
 ]
