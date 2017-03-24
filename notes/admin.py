1cb9aa3c85f271adaf14c8928d5424deec15ca68
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-24 12:05:45 +0200

    use CharFieldUpper in admin interface

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.forms import ModelForm, ValidationError, Textarea
 
 from notes.models import Note
+from notes.forms import CharFieldUpper
 
 
-admin.site.register(Note)
+class NoteFormAdmin(ModelForm):
+    text = CharFieldUpper(
+        widget=Textarea(attrs={
+            'class': 'form-control',
+            'cols': 80
+        })
+    )
+
+class NoteAdmin(admin.ModelAdmin):
+    form = NoteFormAdmin
+
+admin.site.register(Note, NoteAdmin)
