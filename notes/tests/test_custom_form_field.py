c3b86f92b40cb9c2d2d957c10dd977444af81d9e
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-24 16:58:19 +0200

    update tests for future ajax

--- notes/tests/test_custom_form_field.py
+++ notes/tests/test_custom_form_field.py
@@ -8,11 +8,13 @@ from notes.forms import MIN_LEN_NOTE
 class TestNoteAddFormCustomField(TestCase):
     def setUp(self):
         self.client = Client()
-        self.url = reverse('notes_add_upper')
+        self.url = reverse('home')
 
     def test_form(self):
         # get response
-        response = self.client.get(self.url)
+        response = self.client.get(self.url, {
+            "add_note_link": True
+        }, follow=False)
 
         # check response status
         self.assertEqual(response.status_code, 200)
@@ -24,8 +26,9 @@ class TestNoteAddFormCustomField(TestCase):
 
     def test_success(self):
         response = self.client.post(self.url, {
-            'text': "Field Text filed"
-            }, follow=True
+            'text': "Field Text filed",
+            'add_button': True
+            }, follow=False
         )
 
         self.assertEqual(response.status_code, 200)
@@ -33,27 +36,25 @@ class TestNoteAddFormCustomField(TestCase):
         self.assertEqual(note.text, "FIELD TEXT FILED")
         self.assertIn(b'Note uppercase create successfully', response.content)
 
-        self.assertEqual(response.redirect_chain[0][0], '/')
-
     def test_cancel(self):
         value_notes = Note.objects.all().count()
         response = self.client.post(self.url, {
             'text': "Field",
             'cancel_button': True
-            }, follow=True
+            }, follow=False
         )
 
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Note uppercase is not create", response.content)
 
-        self.assertEqual(response.redirect_chain[0][0], '/')
         self.assertEqual(value_notes, Note.objects.all().count())
 
     def test_less_ten_char(self):
         value_notes = Note.objects.all().count()
         response = self.client.post(self.url, {
-            'text': "Field"
-            }, follow=True
+            'text': "Field",
+            'add_button': True
+            }, follow=False
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn('The text field required at least {} characters'.
