c543ff287140ab42c8982b13f3ffef0a655baabe
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-28 00:54:37 +0300

    pep8 pyflakes

--- notes_proj/middleware.py
+++ notes_proj/middleware.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from notes.models import RequestMy
 from django.core.urlresolvers import reverse
 
@@ -7,10 +6,6 @@ class RequestMyMiddleware(object):
     """Middleware for storing requests"""
 
     def process_request(self, request):
-        # print(request.path)
-        # print(reverse('requests_ajax'))
         if request.path != reverse('requests_ajax'):
             RequestMy.objects.create(path=request.path, method=request.method)
-        # print(request.method)
-        # print(datetime.now())
         return None
