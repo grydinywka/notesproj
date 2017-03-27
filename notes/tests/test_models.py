ab968039ada9c6f48dc588941b89eb007761de2e
Sergiy Ignatenko <grydinywka@gmail.com>
2017-03-27 17:26:20 +0300

    make tests

--- notes/tests/test_models.py
+++ notes/tests/test_models.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 
-from notes.models import Note
+from notes.models import Note, Book
 
 
-class NotesModelTest(TestCase):
-    """Test Note Model"""
+class ModelsTest(TestCase):
+    """Test for Models"""
 
-    def test_str(self):
+    def test_str_note(self):
         note = Note.objects.create(text="Buy bread, pasta, oranges.")
         note1 = Note.objects.create(text="Buy bread")
         note2 = Note.objects.create(text="Buy")
@@ -16,3 +16,28 @@ class NotesModelTest(TestCase):
         self.assertEqual(str(note1), "Buy bread")
         self.assertEqual(str(note2), "Buy")
         self.assertEqual(str(note3), "")
+
+    def test_book(self):
+        note1 = Note.objects.create(text="Buy bread")
+        note2 = Note.objects.create(text="Buy")
+        book = Book.objects.create()
+
+        book.notes.add(note1)
+        book.notes.add(note2)
+
+        self.assertIn(note1, book.notes.all())
+        self.assertIn(note2, book.notes.all())
+
+    def test_book_drop_notes(self):
+        book_count = Book.objects.count()
+        note1 = Note.objects.create(text="Buy bread")
+        note2 = Note.objects.create(text="Buy")
+        book = Book.objects.create()
+
+        book.notes.add(note1)
+        book.notes.add(note2)
+
+        book.notes.remove(note1)
+        book.notes.remove(note2)
+
+        self.assertEqual(book_count, Book.objects.count())
